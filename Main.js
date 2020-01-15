53bcc32f5ef27679cceee475c8921cb84664debc
jpmontgo <jpmontgomery2000@gmail.com>
2020-01-15 03:04:46 -0500

    Test commit of files

---
+++ Main.js
@@ -0,0 +1,56 @@
+function playa(){
+	   var audio = document.getElementById("A");
+    if (audio.paused) {
+        audio.play();
+    }else{
+        audio.currentTime = 0
+    }
+}
+function playb(){
+	   var audio = document.getElementById("B");
+    if (audio.paused) {
+        audio.play();
+    }else{
+        audio.currentTime = 0
+    }
+}
+function playc(){
+	   var audio = document.getElementById("C");
+    if (audio.paused) {
+        audio.play();
+    }else{
+        audio.currentTime = 0
+    }
+}
+function playd(){
+	   var audio = document.getElementById("D");
+    if (audio.paused) {
+        audio.play();
+    }else{
+        audio.currentTime = 0
+    }
+}
+function playe(){
+	   var audio = document.getElementById("E");
+    if (audio.paused) {
+        audio.play();
+    }else{
+        audio.currentTime = 0
+    }
+}
+function playf(){
+	   var audio = document.getElementById("F");
+    if (audio.paused) {
+        audio.play();
+    }else{
+        audio.currentTime = 0
+    }
+}
+function playg(){
+	   var audio = document.getElementById("G");
+    if (audio.paused) {
+        audio.play();
+    }else{
+        audio.currentTime = 0
+    }
+}
